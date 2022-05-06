0cc4762617bb6de54c9c63be7aa94963a91aa966
sohan <sohanzid@gmail.com>
2022-05-06 22:23:44 +0600

    branch 1

---
+++ app/src/main/java/brpack/B.java
@@ -0,0 +1,17 @@
+package brpack;
+
+public class B {
+    int id ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int x) {
+        this.id = id;
+    }
+
+    public B(int p) {
+        this.id = id;
+    }
+}
