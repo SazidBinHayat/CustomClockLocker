684f277eb09d776a0bd22cea7a838c64ab0a1060
sohan <sohanzid@gmail.com>
2022-05-06 22:17:31 +0600

    First commit

---
+++ app/src/main/java/first/A.java
@@ -0,0 +1,14 @@
+package first;
+
+public class A {
+
+    int pass;
+
+    public int getPass() {
+        return pass;
+    }
+
+    public void setPass(int p) {
+        this.pass = pass;
+    }
+}
